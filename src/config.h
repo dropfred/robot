fa715fc8a08f2a8521b564e300882f4f7cab471d
dropfred <robot@fred.net>
2024-04-03 19:27:27 +0200

    zero default slowdown

--- src/config.h
+++ src/config.h
@@ -3,16 +3,16 @@
 
 #include <cstdint>
 
-// #define MECANUM_FL {26, 25, true}
-// #define MECANUM_FR {17, 16, false}
-// #define MECANUM_RL {33, 32, true}
-// #define MECANUM_RR {19, 18, false}
+#define MECANUM_FL {26, 25, true}
+#define MECANUM_FR {17, 16, false}
+#define MECANUM_RL {33, 32, true}
+#define MECANUM_RR {19, 18, false}
 
 // fred
-#define MECANUM_FL {26, 25, true, 0.6f}
-#define MECANUM_FR {17, 16, false}
-#define MECANUM_RL {33, 32, true, 0.6f}
-#define MECANUM_RR {19, 18, false}
+// #define MECANUM_FL {26, 25, true, 0.6f}
+// #define MECANUM_FR {17, 16, false}
+// #define MECANUM_RL {33, 32, true, 0.6f}
+// #define MECANUM_RR {19, 18, false}
 
 #define MECANUM_SPEED_LOW  0.5f
 #define MECANUM_SPEED_HIGH 1.0f
