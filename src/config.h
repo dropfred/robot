84aee27fd6a01bac8d48eae996cba38dad018caa
dropfred <robot@fred.net>
2024-04-04 19:04:25 +0200

    boot led

--- src/config.h
+++ src/config.h
@@ -16,13 +16,20 @@
 #define MECANUM_SPEED_LOW  0.5f
 #define MECANUM_SPEED_HIGH 1.0f
 
+// tools (motor driver) pins
+#define TOOL_MAGNET_UPDOWN {4, 2, false, 1.0f, false}
+#define TOOL_BONUS_UPDOWN {27, 13, false, 1.0f, false}
+
+// claw (servo) command pin
+#define TOOL_BONUS_CLAW 22
+
 // bluetooth / serial module pins
 #ifdef HM10_SERIAL
 #define HM10_SERIAL_RX 35
 #define HM10_SERIAL_TX 23
 #endif
 
-// claw (servo) command pin
-#define CLAW_CMD 22
+// boot led pin
+#define DBG_BOOT 21
 
 #endif
